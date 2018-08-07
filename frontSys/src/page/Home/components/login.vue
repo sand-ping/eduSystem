c1e7e6a72b500602bb6c706198c2dc30f7c46681
jiangzeping <1092116709@qq.com>
2018-08-08 00:42:07 +0800

    增加首页

--- frontSys/src/page/Home/components/login.vue
+++ frontSys/src/page/Home/components/login.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="wrapper">
-    <div class="loginWrap">
-      <div class="loginWrap-header">
-        <div class="header-stu">学生登录</div>
-        <div class="header-tea">教师登录</div>
+    <div class="bgimage"></div>
+    <div class="centerwrap">
+      <div class="centerWrap-left">
+        <div class="centerWrap-left-image"></div>
       </div>
-      <div class="loginWrap-center">
-        <div class="loginWrap-center-view">
-          <div class="label">帐号：</div>
-          <el-input v-model="user" placeholder="请输入帐号"></el-input>
+      <div class="loginWrap">
+        <div class="loginWrap-header">
+          <div class="loginWrap-header-line">
+            <div class="loginWrap-header-line-left" :style="loginModel==0?'background:#108cee':''"></div>
+            <div class="loginWrap-header-line-right" :style="loginModel==1?'background:#108cee':''"></div>
+          </div>
+          <div class="header-stu">学生登录</div>
+          <div class="header-tea">教师登录</div>
         </div>
-        <div class="loginWrap-center-view">
-          <div class="label">密码：</div>
-          <el-input v-model="password" placeholder="请输入密码"></el-input>
+        <div class="messageWrap">
+          <div class="message">短信快捷登陆</div>
         </div>
+        <div class="loginWrap-center">
+          <el-input v-model="user" placeholder="请输入帐号" class="loginWrap-center-input"></el-input>
+          <el-input v-model="password" placeholder="请输入密码" class="loginWrap-center-input"></el-input>
+        </div>
+        <div class="login">登录</div>
       </div>
-      <el-button type="primary">登录</el-button>
     </div>
+
   </div>
 </template>
 <script>
@@ -24,7 +32,8 @@ export default{
   data(){
     return{
       user:"",
-      password:""
+      password:"",
+      loginModel:0,
     }
   }
 }
